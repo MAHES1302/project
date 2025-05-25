36bb30f8eec97288409781c52f5523132b399cdf
MAHES1302 <maheswari1302@gmail.com>
2025-05-25 10:51:51 +0530

    code added for testing

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,27 @@
+package org.emp;
+
+import org.client.Client;
+import org.company.Company;
+import org.project.Project;
+
+public class Employee {
+     private void empName() {
+		System.out.println("Employee Name:john");
+
+	}
+     public static void main(String[] args) {
+    	 Employee e = new Employee();
+         e.empName();
+         
+         Company c = new Company();
+         c.companyName();
+         
+         Client cl = new Client();
+         cl.clientName();
+         
+         Project p = new Project();
+         p.projectName();
+	}
+    
+}
+ 
